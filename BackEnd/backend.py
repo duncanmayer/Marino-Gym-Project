93e771ed806cb1f975d376557835f609cd55f9ec
duncanmayer <106922547+duncanmayer@users.noreply.github.com>
2022-11-04 20:36:14 -0400

    Update backend.py

--- BackEnd/backend.py
+++ BackEnd/backend.py
@@ -1,8 +1,10 @@
 from flask import Flask
+from flask_cors import CORS
 
 from main import gym_info_to_dict, filter_last_week
 
 app = Flask(__name__)
+CORS(app)
 
 @app.route("/")
 def marino_data():
