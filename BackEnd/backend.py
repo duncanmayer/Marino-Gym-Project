1be08a6e582f8d9d1dad412875a4c7b193d048e6
talberdichevsky <berdichevsky.t@northeastern.edu>
2022-11-06 13:31:27 -0500

    backend automation changes

--- BackEnd/backend.py
+++ BackEnd/backend.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from flask_cors import render_template
 from flask_cors import CORS
 
 from main import gym_info_to_dict, filter_last_week
@@ -9,6 +10,10 @@ CORS(app)
 @app.route("/")
 def marino_data():
     return gym_info_to_dict(filter_last_week())
+
+@app.route("/home")
+def display_home():
+    return render_template('home.html', name)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0")
